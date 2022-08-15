f44c14ec7c91148da5e853d8326ae81b4c0844a1
sachin <sachin.jmu7@gmail.com>
2022-08-15 15:56:07 +0530

    New work monday

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -4,7 +4,7 @@ import {TodoItem} from "../MyComponents/TodoItem";
 export const Todos = (props) => {
   return (
       <div className = "container">
-        <h3>Todos List</h3>
+        <h3>Todos List1</h3>
         {/*{props.todos*/}
         <TodoItem todo={props.todos[0]}/>;
     </div>
